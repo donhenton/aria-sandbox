bd721b7b6807202e8b4dfa527429f02658f919e8
Don Henton <donhenton@gmail.com>
2019-02-11 15:36:46 -0600

    initial setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,64 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ 
 
 var gulp = require('gulp');
+var targetLocation = './public_html/'
+var appDependencies = require('./package.json').dependencies;
+var SASS_FILES = [ './src/sass/*.scss','./src/sass/components/*.scss']; 
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var fs = require('fs');
+var del = require('del');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
-gulp.task('default', function () {
-  // place code for your default task here
+ 
+//https://browsersync.io/docs/gulp         
+
+gulp.task('clean', function (  ) {
+
+    del(['target','public_html/css/main.css']);
+
 });
+
+
+
+gulp.task('compile-sass', function() {
+	return gulp.src('./src/sass/main.scss')
+	  .pipe(sourcemaps.init())
+	  .pipe(sass().on('error', sass.logError))
+	  .pipe(sourcemaps.write())
+		//.pipe(autoprefixer({
+		//	browsers: ['last 2 versions', 'Explorer 9']
+		//}))
+		.pipe(sass())
+		.pipe(concat('main.css'))
+            // .pipe(uglifycss())
+            .pipe(gulp.dest('./public_html/css/'))
+		//.pipe(browserSync.reload({stream: true}))
+           // .pipe(browserSync.stream());
+});
+
+
+gulp.task('serve', ['compile-sass'], function() {
+
+    browserSync.init({
+        server: "./public_html",
+        port: 6600,
+        ui: {port: 6601}
+    });
+
+    gulp.watch(SASS_FILES, ['compile-sass'] ) ;
+    gulp.watch("public_html/css/main.css").on('change', browserSync.reload);
+    gulp.watch("public_html/samples/grids/**/*.css").on('change', browserSync.reload);
+    gulp.watch("public_html/**/*.html").on('change', browserSync.reload);
+    gulp.watch("public_html/js/**/*.js").on('change', browserSync.reload);
+});
+
+ 
+
+gulp.task('init-dev',[ 'compile-sass']);
+gulp.task('default', ['serve']);
+
+/* end frontend task ---------------------------------------- */
+
