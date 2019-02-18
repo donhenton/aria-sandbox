1e42c2c530d6f79180de91ea95e3415429f7bfdf
Don Henton <donhenton@gmail.com>
2019-02-18 14:53:23 -0600

    working on animation

--- public_html/js/samples/animatedList.js
+++ public_html/js/samples/animatedList.js
@@ -1,49 +1,79 @@
 
-/**
- * @namespace aria
- */
-
-var aria = aria || {};
-
-/**
- * @desc
- *  Key code constants
- */
-aria.KeyCode = {
-  BACKSPACE: 8,
-  TAB: 9,
-  RETURN: 13,
-  ESC: 27,
-  SPACE: 32,
-  PAGE_UP: 33,
-  PAGE_DOWN: 34,
-  END: 35,
-  HOME: 36,
-  LEFT: 37,
-  UP: 38,
-  RIGHT: 39,
-  DOWN: 40,
-  DELETE: 46
-};
-
-//aria.Utils = aria.Utils || {};
 
 
+class AnimatedList {
+  constructor(elem) {
 
- /*
-   * 
-   * 
-   * ANIMATED LIST CLASS
-   * 
-   * 
-   */
+    this.listBox = elem;
+    this.KeyCode = {
+      BACKSPACE: 8,
+      TAB: 9,
+      RETURN: 13,
+      ESC: 27,
+      SPACE: 32,
+      PAGE_UP: 33,
+      PAGE_DOWN: 34,
+      END: 35,
+      HOME: 36,
+      LEFT: 37,
+      UP: 38,
+      RIGHT: 39,
+      DOWN: 40,
+      DELETE: 46
+    };
+
+    this.listBox.addEventListener('focus', this.setupFocus.bind(this));
+    this.listBox.addEventListener('keydown', this.checkKeyPress.bind(this));
+    this.listBox.addEventListener('click', this.checkClickItem.bind(this));
+    this.listItems = Array.from(this.listBox.querySelectorAll('[role="option"]'));
+    this.selectIndex = 0;
+    let me = this;
+    this.listItems.forEach((e, idx) => {
+      e.setAttribute('id', me.idxToid(idx));
+    })
 
 
 
 
-
-  class AnimatedList {
-    constructor(elem) {
-      this.listBox = elem;
-    }
   }
+  idxToId(idx) {
+    return `animated-select-${idx}`;
+  }
+  idToIdx(id) {
+    let foundId;
+    let activeDescendent = this.listBox.getAttribute('aria-activedescendant');
+    if (activeDescendent) {
+
+      this.listItems.forEach((e, idx) => {
+        if (e.getAttribute('id') === activeDescendent) {
+          foundId = idx;
+        }
+      })
+
+    }
+
+
+  }
+  setupFocus(ev) {
+    ev.preventDefault();
+    let activeDescendent = this.listBox.getAttribute('aria-activedescendant');
+    if (!activeDescendent) {
+      this.listItems[this.selectIndex].classList.add('selected')
+      this.listBox.setAttribute('aria-activedescendant', this.idxToId(this.selectIndex));
+    }
+
+  }
+  checkClickItem(evt) {
+    evt.preventDefault();
+    console.log(evt.target.parentElement)
+  }
+  checkKeyPress(evt) {
+    console.log(evt);
+  }
+  setHandleFocusChange(f) {
+
+  }
+  focusOnItem(element) {
+
+  }
+}
--- public_html/js/samples/animatedSelect.js
+++ public_html/js/samples/animatedSelect.js
@@ -7,23 +7,54 @@
       this.animatedList = new AnimatedList(this.selectBox.querySelector('ul.source-selector'))
       this.button = elem.querySelector('button');
       this.stage = elem.querySelector('.select-stage');
+      let bndShow = this.showListbox.bind(this);
       this.button.addEventListener('click', this.showListbox.bind(this));
+      this.button.addEventListener('keyup', this.checkShow.bind(this));
+      this.animatedList.listBox.addEventListener('blur', this.hideListbox.bind(this));
+      this.animatedList.listBox.addEventListener('keydown', this.checkHide.bind(this));
+      this.animatedList.setHandleFocusChange(this.onFocusChange.bind(this));
       this.stage.addEventListener('transitionend', this.stageListener.bind(this));
       this.done = false;
       this.endCounter = 0;
       this.animating = false;
+      this.currentValue = '';
 
     }
+    checkShow(evt) {
+      var key = evt.which || evt.keyCode;
+
+      switch (key) {
+        case this.animatedList.KeyCode.UP:
+        case this.animatedList.KeyCode.DOWN:
+          evt.preventDefault();
+          this.showListbox(evt);
+          this.animatedList.checkKeyPress(evt);
+          break;
+      }
+    }
+    checkHide(evt) {
+      var key = evt.which || evt.keyCode;
+
+      switch (key) {
+        case this.animatedList.KeyCode.RETURN:
+        case this.animatedList.KeyCode.ESC:
+          evt.preventDefault();
+          this.hideListbox(evt);
+
+          break;
+      }
+    }
     stageListener(ev) {
-      
-      
+
+
       let isStage = Array.from(ev.srcElement.classList).includes('select-stage');
       if (isStage === true) {
         this.endCounter++;
       }
-      
-     // console.log(`isStage ${isStage} done ${this.done} counter ${this.endCounter} prop ${ev.propertyName}`)
+
+     //  console.log(`isStage ${isStage} done ${this.done} counter ${this.endCounter} prop ${ev.propertyName}`)
       let me = this;
+      //done == true means drop list is open
       if (isStage && this.endCounter === 5 && this.done === true) {
         this.animatedList.listBox.classList.add('completed');
         window.setTimeout(() => {
@@ -31,37 +62,47 @@
           me.animating = false;
         }, 15);
         this.endCounter = 0;
+        this.animatedList.listBox.focus();
       }
       if (isStage && this.endCounter === 5 && this.done === false) {
-         this.animatedList.listBox.classList.remove('vis');
-         this.animatedList.listBox.classList.remove('completed');
+        this.animatedList.listBox.classList.remove('vis');
+        this.animatedList.listBox.classList.remove('completed');
         this.endCounter = 0;
+        this.button.focus();
       }
-      
+
       //console.log(ev)
     }
     showListbox(ev) {
 
       ev.preventDefault();
       this.animating = true;
+      this.stage.classList.add("completed")
+      this.button.setAttribute('aria-expanded', 'true');
+      this.done = true;
 
-      if (this.done) {
-      
-         window.setTimeout(() => {
-           this.stage.classList.remove('completed')
-         }, 15)
-       
-        this.done = false;
-      } else {
-        this.stage.classList.add("completed")
-        this.done = true;
-      }
+      // }
 
+    }
+    hideListbox(ev) {
+
+      ev.preventDefault();
+      this.animating = true;
+      this.stage.classList.remove('completed');
+      this.button.removeAttribute('aria-expanded');
+      this.done = false;
+
+    }
+    onFocusChange(focusedItem) {
+      this.button.innerText = focusedItem.innerText;
+      this.currentValue = this.button.innerText;
     }
   }
 
-
  
+
+
+
 
 
 
